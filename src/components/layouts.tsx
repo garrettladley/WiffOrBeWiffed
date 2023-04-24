5644bf6a7a92f760c7c118a9e66653fc3b71bd17
garrettladley <ladley.g@northeastern.edu>
2023-04-23 20:52:37 -0400

    sign in text instead of empty profile picture | refactored layout.tsx component

--- src/components/layouts.tsx
+++ src/components/layouts.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 
 import { SignInButton, useUser } from '@clerk/nextjs';
@@ -6,29 +7,16 @@ import { Transition } from '@headlessui/react';
 
 import type { PropsWithChildren } from "react";
 
-const ProfilePicture = () => {
+const Profile = () => {
   const { user, isSignedIn } = useUser();
 
   return (
     <>
       {!isSignedIn && (
         <SignInButton>
-          <div className="flex items-center justify-center p-2">
-            <div className="relative h-6 w-6 overflow-hidden rounded-full bg-gray-100 dark:bg-gray-600">
-              <svg
-                className="absolute -left-1 h-8 w-8 text-gray-400"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </div>
-          </div>
+          <a className="rounded-md px-3 py-2 text-sm font-medium text-white hover:bg-gray-700">
+            Sign in
+          </a>
         </SignInButton>
       )}
       {isSignedIn && (
@@ -36,7 +24,7 @@ const ProfilePicture = () => {
           <Image
             src={user.profileImageUrl}
             alt="Profile Image"
-            className="h-14 w-14 rounded-full"
+            className="h-10 w-10 rounded-full"
             width={56}
             height={56}
           />
@@ -46,10 +34,92 @@ const ProfilePicture = () => {
   );
 };
 
+const routes = [
+  { href: "/", label: "Home" },
+  { href: "/baz", label: "Baz" },
+];
+
+const isActiveBig = (href: string) => {
+  const activeStyles =
+    "rounded-md px-3 py-2 text-sm font-medium text-white hover:bg-gray-700";
+  const inactiveStyles =
+    "rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white";
+  return useRouter().pathname === href ? activeStyles : inactiveStyles;
+};
+
+const isActiveSmall = (href: string) => {
+  const activeStyles =
+    "block rounded-md px-3 py-2 text-base font-medium text-white hover:bg-gray-700";
+  const inactiveStyles =
+    "block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white";
+  return useRouter().pathname === href ? activeStyles : inactiveStyles;
+};
+
+function renderRoutes(isActiveFn: (href: string) => string) {
+  return routes.map((route) => (
+    <a href={route.href} className={isActiveFn(route.href)}>
+      {route.label}
+    </a>
+  ));
+}
+
+const wiffleBall = (
+  <div className="flex-shrink-0">
+    <Image
+      src="/assets/ball-art/white-ball-transparent-bg.png"
+      alt="Ball"
+      width={56}
+      height={56}
+    />
+  </div>
+);
+
+const bigRoutes = (
+  <div className="hidden md:block">
+    <div className="ml-10 flex items-baseline space-x-4">
+      {renderRoutes(isActiveBig)}
+    </div>
+  </div>
+);
+
+type Social = {
+  href: string;
+  svg: React.ReactNode;
+};
+
+const socials: Social[] = [
+  {
+    href: "https://www.instagram.com/wiff_or_be_wiffed/",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-10 w-10"
+        fill="white"
+        viewBox="0 0 24 24"
+      >
+        <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
+      </svg>
+    ),
+  },
+  {
+    href: "https://github.com/garrettladley/WiffOrBeWiffed",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-10 w-10"
+        fill="white"
+        viewBox="0 0 24 24"
+      >
+        <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+      </svg>
+    ),
+  },
+];
+
 export const PageLayout = (props: PropsWithChildren) => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const { isLoaded: userLoaded, isSignedIn } = useUser();
+  const { isLoaded: userLoaded } = useUser();
 
   if (!userLoaded) return <div />;
 
@@ -60,82 +130,16 @@ export const PageLayout = (props: PropsWithChildren) => {
           <div className="max-w-7xl px-4 sm:px-6 lg:px-8">
             <div className="flex h-16 items-center justify-between">
               <div className="flex items-center">
-                <div className="flex-shrink-0">
-                  <Image
-                    src="/assets/ball-art/white-ball-transparent-bg.png"
-                    alt="Ball"
-                    width={40}
-                    height={40}
-                  />
-                </div>
-                <div className="hidden md:block">
-                  <div className="ml-10 flex items-baseline space-x-4">
-                    <a
-                      href="#"
-                      className=" rounded-md px-3 py-2 text-sm font-medium text-white hover:bg-gray-700"
-                    >
-                      Dashboard
-                    </a>
-
-                    <a
-                      href="#"
-                      className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                    >
-                      Team
-                    </a>
-
-                    <a
-                      href="#"
-                      className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                    >
-                      Projects
-                    </a>
-
-                    <a
-                      href="#"
-                      className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                    >
-                      Calendar
-                    </a>
-
-                    <a
-                      href="#"
-                      className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                    >
-                      Reports
-                    </a>
-                  </div>
-                </div>
+                {wiffleBall}
+                {bigRoutes}
                 <div className="absolute right-0 flex items-center justify-center p-4 text-xl">
-                  <a
-                    href="https://instagram.com/wiff_or_be_wiffed?igshid=YmMyMTA2M2Y=
-https://instagram.com/wiff_or_be_wiffed?igshid=YmMyMTA2M2Y="
-                  >
+                  {socials.map((social) => (
                     <div className="flex items-center justify-center p-2">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-6 w-6"
-                        fill="white"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
-                      </svg>
+                      <a href={social.href}>{social.svg}</a>
                     </div>
-                  </a>
-                  <a href="https://github.com/garrettladley/WiffOrBeWiffed">
-                    <div className="flex items-center justify-center p-2">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-6 w-6"
-                        fill="white"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-                      </svg>
-                    </div>
-                  </a>
-                  <ProfilePicture />
-                  <div className="flex items-center justify-center p-2 md:hidden">
+                  ))}
+                  <Profile />
+                  <div className="flex items-center justify-between p-2 md:hidden">
                     <button
                       onClick={() => setIsOpen(!isOpen)}
                       type="button"
@@ -195,40 +199,7 @@ https://instagram.com/wiff_or_be_wiffed?igshid=YmMyMTA2M2Y="
           >
             <div className="md:hidden" id="mobile-menu">
               <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-                <a
-                  href="#"
-                  className="block rounded-md px-3 py-2 text-base font-medium text-white hover:bg-gray-700"
-                >
-                  Dashboard
-                </a>
-
-                <a
-                  href="#"
-                  className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                >
-                  Team
-                </a>
-
-                <a
-                  href="#"
-                  className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                >
-                  Projects
-                </a>
-
-                <a
-                  href="#"
-                  className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                >
-                  Calendar
-                </a>
-
-                <a
-                  href="#"
-                  className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                >
-                  Reports
-                </a>
+                {renderRoutes(isActiveSmall)}
               </div>
             </div>
           </Transition>
